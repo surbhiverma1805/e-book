4c00dc73990464d538ef29aa151a1498a7cf0598
surbhiverma1805 <surbhiverma1805@gmil.com>
2023-04-19 18:55:35 +0530

    Pdf view changes.

--- lib/src/screen/pdf_view/aspect_ratio_fraction.dart
+++
@@ -1,8 +0,0 @@
-class AspectRatioFraction {
-  final double widthFactor;
-  final double heightFactor;
-
-  double get value => widthFactor / heightFactor;
-
-  const AspectRatioFraction(this.widthFactor, this.heightFactor);
-}
